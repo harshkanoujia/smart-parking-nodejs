c4967c89a8e5e56e79bd1051302ef35c9afd56ed
Harsh kanoujia <kanoujia.9harsh@gmail.com>
2025-07-06 02:05:03 +0530

    fix: seed

--- startup/seed.js
+++ startup/seed.js
@@ -1,19 +1,24 @@
+const config = require("config");
 const { Admin } = require('../models/Admin');
 
+
 async function Seed() {
-  let admin = await Admin.find()
-  if (admin.length === 0) {
-    await Admin.insertMany([
-      {
-        email: "admin@pms.com",
-        password: "$2b$10$oPd2gJERglqNiWXf3sYwIubzrVRrt25/wJP.wx9GEDiVAS6xDzA8y"
-      },
-      {
-        email: "pms@admin.com",
-        password: "$2b$10$oPd2gJERglqNiWXf3sYwIubzrVRrt25/wJP.wx9GEDiVAS6xDzA8y"
-      },
-    ])
-  }
+
+    let admin = await Admin.find();
+
+    if (admin.length === 0) {
+        await Admin.insertMany([
+            {
+                email: config.get("default_seed_admin_1"),
+                password: config.get("default_seed_admin_password"),
+            },
+            {
+                email: config.get("default_seed_admin_2"),
+                password: config.get("default_seed_admin_password"),
+            },
+        ])
+    }
 }
+
 
 module.exports.Seed = Seed;
